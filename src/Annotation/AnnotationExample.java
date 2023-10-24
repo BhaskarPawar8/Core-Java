3c41c8c93f7e5710954262bf7b6545363ff5f57c
bhaskar pawar <bhaskar.pawar8@gmail.com>
2023-10-24 22:13:24 +0100

    builder pattern

---
+++ src/Annotation/AnnotationExample.java
@@ -0,0 +1,24 @@
+package Annotation;
+
+import java.lang.reflect.Method;
+
+public class AnnotationExample {
+
+    public static void main(String[] args) throws NoSuchMethodException {
+
+        Class<MyClass> clazz = MyClass.class;
+
+        Method method = clazz.getMethod("myMethod");
+
+        MyAnnotation myAnnotation = method.getAnnotation(MyAnnotation.class);
+
+        if(myAnnotation != null){
+            
+
+            String value = myAnnotation.value();
+            System.out.println("Value: "+ value);
+        }
+
+    }
+
+}
---
+++ src/Annotation/MyClass.java
@@ -0,0 +1,17 @@
+package Annotation;
+
+import java.lang.annotation.Annotation;
+
+public class MyClass{
+
+    @MyAnnotation("custome valu")
+    public void myMethod(){
+
+    }
+    @MyAnnotation
+    public void anotherMethod(){
+
+    }
+
+
+}
